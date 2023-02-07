9e3d5551a124ce5b9c34a90f465b899d2ad66e69
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2023-02-06 18:07:53 -0700

    Fix div CSS to prior to before numbers were added to each ID upon creation

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ document.addEventListener('click', createDivs);
 document.addEventListener('mouseover', colorSquare);
 
 function createDivs () {
-    // first loop is for the number of rows
     let numberColumns = 0;
     let numberRows = 0;
     while (numberColumns == 0 || numberColumns > 100) {
@@ -11,21 +10,26 @@ function createDivs () {
     while (numberRows == 0 || numberRows > 100) {
         numberRows = prompt("How many ROWS would you like?");
     }
+    // first loop is for the number of rows
     for (let i = 0; i < numberRows; i++) {
         const rowDiv = document.createElement('div');
-        rowDiv.setAttribute('id', `divRow`)
+        rowDiv.setAttribute('class', 'divRow');
+        rowDiv.setAttribute('id', `divRow${i}`);
         document.getElementById('etch-container').appendChild(rowDiv);
-        // second loop creates x divs inside the first row
+        // second loop creates j divs inside i row
         for (let j = 0; j < numberColumns; j++) {
             const colDiv = document.createElement('div');
-            colDiv.setAttribute('id', `divCol`)
+            colDiv.setAttribute('class', 'divCol');
+            colDiv.setAttribute('id', `divCol${j}`);
             rowDiv.appendChild(colDiv);
         }
     }
 }
 
-function colorSquare() {
-
+function colorSquare(e) {
+    // document.getElementById(this.id).style.backgroundColor = 'red';
+    e.stopPropagation();
+    console.log(this);
 }
 
 
