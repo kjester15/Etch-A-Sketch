e687a3268d2fb42be36fbe6e56d60c6384cdbc30
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2023-02-06 17:11:00 -0700

    Add row and column limit of 100 and restrict user from entering 0

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 document.addEventListener('click', createDivs);
+document.addEventListener('mouseover', colorSquare);
 
 function createDivs () {
     // first loop is for the number of rows
-    const numberColumns = prompt("How many COLUMNS would you like?");
-    const numberRows = prompt("How many ROWS would you like?");
+    let numberColumns = 0;
+    let numberRows = 0;
+    while (numberColumns == 0 || numberColumns > 100) {
+        numberColumns = prompt("How many COLUMNS would you like?");
+    }
+    while (numberRows == 0 || numberRows > 100) {
+        numberRows = prompt("How many ROWS would you like?");
+    }
     for (let i = 0; i < numberRows; i++) {
         const rowDiv = document.createElement('div');
         rowDiv.setAttribute('id', `divRow`)
@@ -15,6 +22,10 @@ function createDivs () {
             rowDiv.appendChild(colDiv);
         }
     }
+}
+
+function colorSquare() {
+
 }
 
 
