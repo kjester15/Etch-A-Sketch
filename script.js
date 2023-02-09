76158c79b2bb2ad3c3581f0432cd0183d8d81da8
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2023-02-08 17:41:03 -0700

    Added barebones color picker html and css

--- script.js
+++ script.js
@@ -8,17 +8,18 @@ function restart () {
 function createDivs () {
     let numberColumns = document.getElementById('sizeSlider').value;
     let numberRows = document.getElementById('sizeSlider').value;
+    
     // while (numberColumns == 0 || numberColumns > 100) {
     //     numberColumns = prompt("How many COLUMNS would you like?");
     // }
     // while (numberRows == 0 || numberRows > 100) {
     //     numberRows = prompt("How many ROWS would you like?");
     // }
+
     // first loop is for the number of rows
     for (let i = 0; i < numberRows; i++) {
         const rowDiv = document.createElement('div');
         rowDiv.setAttribute('class', 'divRow');
-        // rowDiv.setAttribute('id', `divRow${i}`);
         document.getElementById('etch-container').appendChild(rowDiv);
         // second loop creates j divs inside i row
         for (let j = 0; j < numberColumns; j++) {
